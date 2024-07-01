1461914d36946a9b653f643b44aea0d2eaf579fd
DESKTOP-RIQ9KO3\dev <dev@email>
2024-07-01 22:09:48 +0200

    Other commentaires

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -3,7 +3,7 @@ package com.wakaleo.gameoflife.domain;
  /**
  * Wakaleo Consulting - John Ferguson Smart
  * Game of life, demonstration application for Jenkins: The Definitive Guide
- * New comment & other comment
+ * New comment & other commentaires
  *
  * Cell.java
  * Cell object class, contains information on individual cells (positions) in the grid
